6a2078fd28d7cbe7c675c06b795142f7d9f1bf96
ErikSeewald <erik.seewaldx@gmail.com>
2025-01-15 11:14:46 +0100

    option to edit all wasps at once

--- Eriks_Wasp_Sim/src/input/console/WaspCommandHandlers.cpp
+++ Eriks_Wasp_Sim/src/input/console/WaspCommandHandlers.cpp
@@ -3,6 +3,7 @@
 #include "StringUtil.h"
 #include "CommandUtil.h"
 #include "UI.h"
+#include "WaspSlots.h"
 
 static const std::string waspCommandName = "wasp";
 CommandHandlerMap waspCommandHandlers =
@@ -21,7 +22,7 @@ void WaspCommandHandlers::commandWasp(const std::string& subcommand)
 
 void WaspCommandHandlers::commandWaspHelp(const std::string& subcommand)
 {
-    if (!StringUtil::isBlank(subcommand))
+    if (!StringUtil::isBlank(subcommand) && StringUtil::trimLeadingWhitespace(subcommand) != "all")
     {
         CommandUtil::printInvalidSyntaxError();
         return;
@@ -30,9 +31,15 @@ void WaspCommandHandlers::commandWaspHelp(const std::string& subcommand)
     CommandUtil::printSubCommands(waspCommandName);
 }
 
+#include "OtherCommandHandlers.h"
 void WaspCommandHandlers::commandWaspKill(const std::string& subcommand)
 {
     static const std::string successfulKillPrint = "Wasp killed successfully!";
+    if (StringUtil::getFirstWord(subcommand) == "all")
+    { 
+        OtherCommandHandlers::commandKill("wasp all" ); 
+        return; 
+    }
 
     if (!StringUtil::isBlank(subcommand))
     {
@@ -56,7 +63,11 @@ void WaspCommandHandlers::commandWaspSetPos(const std::string& subcommand)
 {
     //POSITION STRING
     std::string posString = StringUtil::getFirstWord(subcommand);
-    if (posString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(subcommand)))
+
+    std::string cutCommand = StringUtil::cutFirstWord(subcommand);
+    bool allWasps = StringUtil::getFirstWord(cutCommand) == "all";
+
+    if (posString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(cutCommand)))
     {
         CommandUtil::printInvalidSyntaxError();
         return;
@@ -71,31 +82,43 @@ void WaspCommandHandlers::commandWaspSetPos(const std::string& subcommand)
     }
 
     //SET POSITION
-    Wasp* selectedWasp = UI::getUIState()->selectedWasp;
-    if (selectedWasp != nullptr)
+    if (allWasps)
     {
-        selectedWasp->position = newPos;
-        std::cout << "Wasp's position set to " << newPos.x << "," << newPos.y << "," << newPos.z;
+        EntitySlot* currentSlot = WaspSlots::getWaspSlots();
+        while (currentSlot != nullptr)
+        {
+            Wasp* wasp = (Wasp*)currentSlot->entity;
+            currentSlot = currentSlot->next;
+
+            wasp->position = newPos;
+        }
+        std::cout << "All wasp positions set to " << newPos.x << "," << newPos.y << "," << newPos.z;
     }
+
     else
     {
-        CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
+        Wasp* selectedWasp = UI::getUIState()->selectedWasp;
+        if (selectedWasp != nullptr)
+        {
+            selectedWasp->position = newPos;
+            std::cout << "Wasp's position set to " << newPos.x << "," << newPos.y << "," << newPos.z;
+        }
+        else
+        {
+            CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
+        }
     }
 }
 
 void WaspCommandHandlers::commandWaspSetHp(const std::string& subcommand)
 {
-    // SELECTED WASP
-    Wasp* selectedWasp = UI::getUIState()->selectedWasp;
-    if (selectedWasp == nullptr)
-    {
-        CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
-        return;
-    }
-
     //HP STRING
     std::string hpString = StringUtil::getFirstWord(subcommand);
-    if (hpString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(subcommand)))
+
+    std::string cutCommand = StringUtil::cutFirstWord(subcommand);
+    bool allWasps = StringUtil::getFirstWord(cutCommand) == "all";
+
+    if (hpString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(cutCommand)))
     {
         CommandUtil::printInvalidSyntaxError();
         return;
@@ -103,29 +126,60 @@ void WaspCommandHandlers::commandWaspSetHp(const std::string& subcommand)
 
     // SET HP
     int newHp = CommandUtil::convertToInt(hpString);
-    int maxHp = selectedWasp->getMaxHP();
-    if (newHp < 0 || newHp > maxHp)
+    if (newHp < 0)
     {
-        CommandUtil::printError("Hp has to be positive and below the selected wasps max hp");
+        CommandUtil::printError("Hp cannot be < 0");
         return;
     }
-    selectedWasp->setHP(newHp);
-    std::cout << "Wasp's hp set to " << newHp;
+
+    if (allWasps)
+    {
+        EntitySlot* currentSlot = WaspSlots::getWaspSlots();
+        while (currentSlot != nullptr)
+        {
+            Wasp* wasp = (Wasp*)currentSlot->entity;
+            currentSlot = currentSlot->next;
+
+            int maxHp = wasp->getMaxHP();
+            int curNewHp = newHp;
+            if (newHp > maxHp)
+            {
+                curNewHp = maxHp;
+            }
+            wasp->setHP(curNewHp);
+        }
+        std::cout << "All wasp's hp set to min(" << newHp << ", their maximum hp)";
+    }
+
+    else
+    {
+        Wasp* selectedWasp = UI::getUIState()->selectedWasp;
+        if (selectedWasp == nullptr)
+        {
+            CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
+            return;
+        }
+
+        int maxHp = selectedWasp->getMaxHP();
+        if (newHp > maxHp)
+        {
+            std::cout << "Desired hp had to be limited to the wasp's max hp" << std::endl;
+            newHp = maxHp;
+        }
+        selectedWasp->setHP(newHp);
+        std::cout << "Wasp's hp set to " << newHp;
+    }
 }
 
 void WaspCommandHandlers::commandWaspSetHunger(const std::string& subcommand)
 {
-    // SELECTED WASP
-    Wasp* selectedWasp = UI::getUIState()->selectedWasp;
-    if (selectedWasp == nullptr)
-    {
-        CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
-        return;
-    }
-
     //HUNGER STRING
     std::string hungerString = StringUtil::getFirstWord(subcommand);
-    if (hungerString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(subcommand)))
+
+    std::string cutCommand = StringUtil::cutFirstWord(subcommand);
+    bool allWasps = StringUtil::getFirstWord(cutCommand) == "all";
+
+    if (hungerString.empty() || !StringUtil::isBlank(StringUtil::cutFirstWord(cutCommand)))
     {
         CommandUtil::printInvalidSyntaxError();
         return;
@@ -133,12 +187,48 @@ void WaspCommandHandlers::commandWaspSetHunger(const std::string& subcommand)
 
     // SET HUNGER SATURATION
     int newSaturation = CommandUtil::convertToInt(hungerString);
-    int maxSaturation = selectedWasp->getMaxHungerSaturation();
-    if (newSaturation < 0 || newSaturation > maxSaturation)
+
+    if (newSaturation < 0)
     {
-        CommandUtil::printError("Hunger saturation has to be positive and below the selected wasps max hunger saturation");
+        CommandUtil::printError("Hunger saturation cannot be < 0");
         return;
     }
-    selectedWasp->setHungerSaturation(newSaturation);
-    std::cout << "Wasp's hunger saturation set to " << newSaturation;
+
+    if (allWasps)
+    {
+        EntitySlot* currentSlot = WaspSlots::getWaspSlots();
+        while (currentSlot != nullptr)
+        {
+            Wasp* wasp = (Wasp*)currentSlot->entity;
+            currentSlot = currentSlot->next;
+
+            int maxSaturation = wasp->getMaxHungerSaturation();
+            int curNewSaturation = newSaturation;
+            if (newSaturation > maxSaturation)
+            {
+                curNewSaturation = maxSaturation;
+            }
+            wasp->setHungerSaturation(curNewSaturation);
+        }
+        std::cout << "All wasp's hunger saturation set to min(" << newSaturation << ", their maximum hunger saturation)";
+    }
+
+    else
+    {
+        Wasp* selectedWasp = UI::getUIState()->selectedWasp;
+        if (selectedWasp == nullptr)
+        {
+            CommandUtil::printError(CommandUtil::noWaspSelectedPrint);
+            return;
+        }
+
+        int maxSaturation = selectedWasp->getMaxHungerSaturation();
+        if (newSaturation > maxSaturation)
+        {
+            std::cout << "Desired hunger saturation had to be limited to the wasp's max hunger saturation" << std::endl;
+            newSaturation = maxSaturation;
+        }
+        selectedWasp->setHungerSaturation(newSaturation);
+        std::cout << "Wasp's hunger saturation set to " << newSaturation;
+    }
 }
