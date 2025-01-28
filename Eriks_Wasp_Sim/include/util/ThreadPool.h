8cfd3acc40edc56a0d1e44dfe3efd1da3601ba77
ErikSeewald <erik.seewaldx@gmail.com>
2025-01-28 12:40:35 +0100

    thread pooling v1

---
+++ Eriks_Wasp_Sim/include/util/ThreadPool.h
@@ -0,0 +1,57 @@
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+/**
+* @object ThreadPool
+* @brief A preinitialized pool of threads which jobs can be assigned to.
+*/
+class ThreadPool
+{
+    public:
+
+        /**
+        * Initialize a pool of the given size that will wait for jobs until the pool is destroyed.
+        */
+        ThreadPool(size_t numThreads);
+
+        ~ThreadPool();
+        
+        /**
+        * Submits the given job to the pool queue. 
+        * The next free worker thread will handle the job.
+        */
+        void enqueue(std::function<void()> job);
+
+        /**
+        * Waits until all jobs in the queue are finished.
+        */
+        void waitFinishAll();
+
+    private:
+
+        /**
+        * The loop that is run by each worker thread. Waits for new jobs, takes care of them
+        * and notifies condition variables if necessary.
+        */
+        void _workerLoop();
+
+        // Used to notify workers of shutdown
+        bool shutDown;
+
+        // WORKERS
+        std::vector<std::thread> workers;
+        std::condition_variable jobAvailableCond; // Condition variable used to wait for new jobs
+
+        // JOB QUEUE
+        std::queue<std::function<void()>> jobs;
+        std::mutex queueMutex;
+
+
+        // WORKER COUNTING
+        std::condition_variable jobsFinishedCond;
+        std::atomic<size_t> activeThreads;      
+};
---
+++ Eriks_Wasp_Sim/src/util/ThreadPool.cpp
@@ -0,0 +1,97 @@
+#include "ThreadPool.h"
+#include <atomic>
+
+/**
+* Initialize a pool of the given size that will wait for jobs until the pool is destroyed.
+*/
+ThreadPool::ThreadPool(size_t numThreads)
+    : shutDown(false)
+    , activeThreads(0)
+{
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        workers.emplace_back([this] {_workerLoop(); });
+    }
+}
+
+ThreadPool::~ThreadPool() 
+{
+    // LOCKED SIGNAL THREADS SHUTDOWN
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        shutDown = true;
+    }
+
+    // Wake all theads so they can exit and then join them
+    jobAvailableCond.notify_all();
+    for (auto& t : workers) 
+    {
+        if (t.joinable()) {t.join();}
+    }
+}
+
+/**
+* Submits the given job to the pool queue.
+* The next free worker thread will handle the job.
+*/
+void ThreadPool::enqueue(std::function<void()> job)
+{
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        jobs.push(std::move(job));
+    }
+    jobAvailableCond.notify_one();
+}
+
+
+/**S
+* Waits until all jobs in the queue are finished.
+*/
+void ThreadPool::waitFinishAll()
+{
+    std::unique_lock<std::mutex> lock(queueMutex);
+
+    jobsFinishedCond.wait(lock, [this] 
+    {
+        return jobs.empty() && (activeThreads == 0);
+    });
+}
+
+/**
+* The loop that is run by each worker thread. Waits for new jobs, takes care of them
+* and notifies condition variables if necessary.
+*/
+void ThreadPool::_workerLoop()
+{
+    std::function<void()> job;
+    while (true)
+    {
+        // LOCKED ACQUIRE JOB LOGIC
+        {
+            // Wait until new job or shutdown
+            std::unique_lock<std::mutex> lock(queueMutex);
+            jobAvailableCond.wait(lock, [this]
+            {
+                return !jobs.empty() || shutDown;
+            });
+
+            if (shutDown && jobs.empty()) { return; }
+
+            // Get next job
+            job = std::move(jobs.front());
+            jobs.pop();
+            ++activeThreads;
+        }
+
+        // RUN JOB
+        job();
+
+        // LOCKED POST JOB LOGIC
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            --activeThreads;
+
+            if (jobs.empty() && activeThreads == 0) { jobsFinishedCond.notify_all(); }
+        }
+    }
+}
